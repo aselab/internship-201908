7c1a6ccfb4a63156c35a68cc302628627c59b80a
SoutaMori-ase <gh1010801310+ase@gmail.com>
2019-07-30 02:04:29 +0900

    付箋の土台作成

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { View, StyleSheet, Text } from 'react-native';
+import StickyNote from './components/StickyNote';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default class App extends React.Component {
+export default class App extends React.Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Hello World
-        </Text>
-        <Card>
-          <AssetExample />
-        </Card>
-      </View>
+        <View style={styles.container}> 
+          <StickyNote />
+       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 40,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+    margin: 24,
+  }  
 });
-
